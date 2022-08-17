1a855bd78fa9d12296c24ea7a1aa6bd702c9f7a5
HenneyKo <stch114@gmail.com>
2022-08-17 17:03:27 +0900

    [ADD, UPDATE] DB 연동 설정

---
+++ ShopMall_SpringMVC/src/main/java/com/dto/MemberDto.java
@@ -0,0 +1,128 @@
+package com.dto;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("MemberDto")
+public class MemberDto {
+
+	private String userid;
+	private String passwd;
+	private String username;
+	private String postcode;
+	private String addr1;
+	private String addr2;
+	private String phone1;
+	private String phone2;
+	private String phone3;
+	private String email1;
+	private String email2;
+
+	public MemberDto() {
+		super();
+	}
+
+	public MemberDto(String userid, String passwd, String username, String postcode, String addr1, String addr2,
+			String phone1, String phone2, String phone3, String email1, String email2) {
+		super();
+		this.userid = userid;
+		this.passwd = passwd;
+		this.username = username;
+		this.postcode = postcode;
+		this.addr1 = addr1;
+		this.addr2 = addr2;
+		this.phone1 = phone1;
+		this.phone2 = phone2;
+		this.phone3 = phone3;
+		this.email1 = email1;
+		this.email2 = email2;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPost() {
+		return postcode;
+	}
+
+	public void setPost(String post) {
+		this.postcode = post;
+	}
+
+	public String getAddr1() {
+		return addr1;
+	}
+
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+
+	public String getAddr2() {
+		return addr2;
+	}
+
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+
+	public String getPhone1() {
+		return phone1;
+	}
+
+	public void setPhone1(String phone1) {
+		this.phone1 = phone1;
+	}
+
+	public String getPhone2() {
+		return phone2;
+	}
+
+	public void setPhone2(String phone2) {
+		this.phone2 = phone2;
+	}
+
+	public String getPhone3() {
+		return phone3;
+	}
+
+	public void setPhone3(String phone3) {
+		this.phone3 = phone3;
+	}
+
+	public String getEmail1() {
+		return email1;
+	}
+
+	public void setEmail1(String email1) {
+		this.email1 = email1;
+	}
+
+	public String getEmail2() {
+		return email2;
+	}
+
+	public void setEmail2(String email2) {
+		this.email2 = email2;
+	}
+
+}
